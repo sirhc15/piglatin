7f462141de3e8d6d58ec1f671818e33a1e1f36f8
oliverbolosan <oliverbolosan@gmail.com>
2018-08-12 19:46:33 -0700

    Adding PigLatin rule engine
    Rule engine to modify english to PigLatin

---
+++ PigLatinEngine/Rules.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PigLatinEngine
+{
+    public class Rules
+    {
+        public bool beginsWithConsonant { get; set; }
+        public bool beginsWithConsonantClusters { get; set; }
+        public bool beginsWithVowelSound { get; set; }
+    }
+}
